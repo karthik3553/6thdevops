d343ba1e99f8474a4af53239c220c6774ef7c151
karthik3553 <akarthik2336@gmail.com>
2025-05-21 11:26:01 +0530

    initial commit-slelenium maven projecgt

---
+++ src/test/java/org/test/LoginTest.java
@@ -0,0 +1,23 @@
+package org.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+    @Test
+    public void testLogin() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/v1/");
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        Thread.sleep(2000);
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        Thread.sleep(2000);
+        driver.findElement(By.id("login-button")).click();
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}
