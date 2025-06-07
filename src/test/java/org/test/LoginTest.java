c4d72a1a6e236d56f117672712ae56ab7ddc50d0
karthik3553 <akarthik2336@gmail.com>
2025-06-08 00:53:18 +0530

    Update LoginTest.java

--- src/test/java/org/test/LoginTest.java
+++ src/test/java/org/test/LoginTest.java
@@ -1,6 +1,6 @@
 package org.test;
 
-import org.openqa.selenium.By
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
